0348bf9d2cb9acff964f7c3e1ae843ab5f70c64f
Julien Biezemans <jb@jbpros.com>
2020-05-06 22:45:56 +0200

    Addition terminée

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,27 +1,28 @@
 import { expect } from 'chai'
 
-describe('calculator', () => {
+describe('calculatrice', () => {
+  beforeEach(() => {
+    x('AC')
+  })
+
   it('retourne un chiffre unique', () => {
     const resultat = x(1)
     expect(resultat).to.equal(1)
   })
 
   it('retourne un nombre à chiffres multiple', () => {
-    memoire = 0
     x(1)
     const resultat = x(5)
     expect(resultat).to.equal(15)
   })
 
   it('retourne le nombre courant après +', () => {
-    memoire = 0
     x(1)
     const resultat = x('+')
     expect(resultat).to.equal(1)
   })
 
   it('retourne le chiffre tapé après +', () => {
-    memoire = 0
     x(1)
     x(5)
     x('+')
@@ -30,7 +31,6 @@ describe('calculator', () => {
   })
 
   it("retourne le résultat de l'addition après =", () => {
-    memoire = 0
     x(1)
     x(5)
     x('+')
@@ -40,7 +40,6 @@ describe('calculator', () => {
   })
 
   it('enchaîne les additions', () => {
-    memoire = 0
     x(1)
     x(5)
     x('+')
@@ -52,19 +51,47 @@ describe('calculator', () => {
     const resultat = x('=')
     expect(resultat).to.equal(57)
   })
+
+  it('considère des opérations intermédiaires', () => {
+    x(1)
+    x('+')
+    x('2')
+    const resultat = x('+')
+    expect(resultat).to.equal(3)
+  })
+
+  it('peut être remise à zéro', () => {
+    x(9)
+    x('+')
+    x('4')
+    expect(x('AC')).to.equal(0)
+    x('1')
+    x('+')
+    x('2')
+    expect(x('=')).to.equal(3)
+  })
 })
 
 let memoire = 0
 let operandeGauche = 0
 
 function x(n: number | string) {
-  if (n === '+') {
+  if (n === '+' && operandeGauche === 0) {
     operandeGauche = memoire
+    memoire = 0
+    return operandeGauche
+  } else if (n === '+') {
+    operandeGauche = operandeGauche + memoire
     memoire = 0
     return operandeGauche
   } else if (n === '=') {
     memoire = operandeGauche + memoire
+    operandeGauche = 0
     return memoire
+  } else if (n === 'AC') {
+    operandeGauche = 0
+    memoire = 0
+    return 0
   } else {
     memoire = memoire * 10 + parseInt(`${n}`)
     return memoire
