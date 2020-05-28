e669d6861810bcddc656f774b6604c9b30bcf0af
Julien Biezemans <jb@jbpros.com>
2020-05-28 22:21:19 +0200

    Extrait le code de production dans un module séparé

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai'
 
+import { x } from '.'
+
 describe('calculatrice', () => {
   beforeEach(() => {
     x('AC')
@@ -172,41 +174,3 @@ describe('calculatrice', () => {
     expect(x('=')).to.equal(3)
   })
 })
-
-let memoire = 0
-let operandeGauche = 0
-let operateur: '+' | '-' | '*' = '+'
-
-function appliqueOperation(): number {
-  if (operateur === '+') {
-    return operandeGauche + memoire
-  } else if (operateur === '-') {
-    return operandeGauche - memoire
-  } else if (operateur === '*') {
-    return operandeGauche * memoire
-  }
-  throw new Error('Opérateur inconnu')
-}
-
-function x(n: number | string) {
-  if (n === '+' || n === '-' || n === '*') {
-    operandeGauche = appliqueOperation()
-    memoire = 0
-    operateur = n
-    return operandeGauche
-  } else if (n === '=') {
-    memoire = appliqueOperation()
-    operandeGauche = 0
-    operateur = '+'
-    return memoire
-  } else if (n === 'AC') {
-    operandeGauche = 0
-    operateur = '+'
-    return (memoire = 0)
-  } else if (n === 'C') {
-    return (memoire = 0)
-  } else {
-    memoire = memoire * 10 + parseInt(`${n}`)
-    return memoire
-  }
-}
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+let memoire = 0
+let operandeGauche = 0
+let operateur: '+' | '-' | '*' = '+'
+
+function appliqueOperation(): number {
+  if (operateur === '+') {
+    return operandeGauche + memoire
+  } else if (operateur === '-') {
+    return operandeGauche - memoire
+  } else if (operateur === '*') {
+    return operandeGauche * memoire
+  }
+  throw new Error('Opérateur inconnu')
+}
+
+export function x(n: number | string) {
+  if (n === '+' || n === '-' || n === '*') {
+    operandeGauche = appliqueOperation()
+    memoire = 0
+    operateur = n
+    return operandeGauche
+  } else if (n === '=') {
+    memoire = appliqueOperation()
+    operandeGauche = 0
+    operateur = '+'
+    return memoire
+  } else if (n === 'AC') {
+    operandeGauche = 0
+    operateur = '+'
+    return (memoire = 0)
+  } else if (n === 'C') {
+    return (memoire = 0)
+  } else {
+    memoire = memoire * 10 + parseInt(`${n}`)
+    return memoire
+  }
+}
